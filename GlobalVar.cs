826429ec2de2de7ef991735298fa3c64ff58fd53
MoonPunch <daniel.osborne90@gmail.com>
2011-08-02 13:54:57 +0100

    Added batch output capture

--- GlobalVar.cs
+++ GlobalVar.cs
@@ -18,13 +18,14 @@ public static class GlobalVar
     // Constants
     public const string BukkitDownload = "http://ci.bukkit.org/job/dev-CraftBukkit/1000/artifact/target/craftbukkit-0.0.1-SNAPSHOT.jar";
     public const string OfficialDownload = "http://www.minecraft.net/download/minecraft_server.jar";
-    public const string javaLocation = @"%ProgramFiles%\Java\jre6\bin\java.exe";
+    public const string javaLocation = @"""%ProgramFiles%\Java\jre6\bin\java.exe""";
     public const string bukkitFileName   = "bukkit0.0.1.jar";
     public const string officialFileName = "minecraft_server.jar";
     public const string batchFileName = @"\server_start.bat";
 
     // Long Term variables
     public static string cleanDir;
+    public static string batchOut;
 
     // Temporary
     public static string tmpServerLink    = "";  // Stores the link to the server before conversion
--- frmMain.cs
+++ frmMain.cs
@@ -33,31 +33,47 @@ namespace McMoonPunch
 
         private void button1_Click(object sender, EventArgs e)
         {
-            using (Process p = new Process())
+            int loop = 1;
+            serverStart();
+            while (loop == 1)
             {
-                p.StartInfo.WorkingDirectory = GlobalVar.cleanDir;
-                p.StartInfo.FileName = GlobalVar.cleanDir+GlobalVar.batchFileName;
-                p.StartInfo.UseShellExecute = false;
-                p.StartInfo.RedirectStandardOutput = true;
-                p.StartInfo.RedirectStandardError = true;
-                p.Start();
-                p.WaitForExit();
-
-                // Capture output from batch file written to stdout and put in the 
-                // RunResults textbox
-                string output = p.StandardOutput.ReadToEnd();
-                if (!String.IsNullOrEmpty(output) && output.Trim() != "")
-                {
-                    this.rtboxServerOut.Text = output;
-                }
-
-                // Capture any errors written to stderr and put in the errors textbox.
-                string errors = p.StandardError.ReadToEnd();
-                if (!String.IsNullOrEmpty(errors) & errors.Trim() != "")
-                {
-                    this.rtboxErrors.Text = errors;
-                }
+                rtboxServerOut.AppendText(GlobalVar.batchOut);
             }
+        }
+
+        private void serverStart()
+        {
+            // Application path and command line arguments
+            string ApplicationPath = GlobalVar.cleanDir + GlobalVar.batchFileName;
+            string ApplicationArguments = "-c -x";
+
+            // Create a new process object
+            Process ProcessObj = new Process();
+
+            // StartInfo contains the startup information of
+            // the new process
+            ProcessObj.StartInfo.FileName = ApplicationPath;
+            ProcessObj.StartInfo.Arguments = ApplicationArguments;
+
+            // These two optional flags ensure that no DOS window
+            // appears
+            ProcessObj.StartInfo.UseShellExecute = false;
+            ProcessObj.StartInfo.CreateNoWindow = false;
+
+            // If this option is set the DOS window appears again :-/
+            // ProcessObj.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            // This ensures that you get the output from the DOS application
+            ProcessObj.StartInfo.RedirectStandardOutput = true;
+
+            // Start the process
+            ProcessObj.Start();
+
+            // Wait that the process exits
+            ProcessObj.WaitForExit();
+
+            // Now read the output of the DOS application
+            GlobalVar.batchOut = ProcessObj.StandardOutput.ReadToEnd();
         }
     }
 }
